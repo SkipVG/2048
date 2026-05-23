21ac5e0ffd870a964138f6e1b64223636b692e9e
SkipVG <veterygaming@gmail.com>
2026-05-23 05:26:00 +0300

    Refactor GameSprite and add Tile class

--- main.py
+++ main.py
@@ -19,22 +19,53 @@ cell_colors = {
     1024: (255, 200, 168),
     2048: (171, 205, 239)
 }
-
+size = 4
+cell_size = 100
+margin = 10
+board_width = size * cell_size + (size - 1) * margin
+board_left = (win_width - board_width) // 2
+board_top = (win_height - board_width) // 2
 font.init()
 font2= font.SysFont('Arial', 36)
 FPS = 60
 game = True
 finish = False
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed):
+    def __init__(self, x, y, width, height, color=None, image=None):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (65, 65))
-        self.speed = player_speed
+        if image:
+            self.image = transform.scale(image.load(image), (width, height))
+        else:
+            self.image = Surface((width, height))
+            if color:
+                self.image.fill(color)
         self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
+        self.rect.x = x
+        self.rect.y = y
+
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Tile(GameSprite):
+    def __init__(self, value, row, col):
+        x = BOARD_LEFT + col * (CELL_SIZE + MARGIN)
+        y = BOARD_TOP + row * (CELL_SIZE + MARGIN)
+        super().__init__(x, y, CELL_SIZE, CELL_SIZE)
+        self.value = value
+        self.update_color()
+
+    def update_color(self):
+        color = cell_colors.get(self.value, (60, 58, 50))
+        self.image.fill(color)
+        # Рисуем текст, если число не ноль
+        if self.value != 0:
+            text = font_small.render(str(self.value), True, (0, 0, 0))
+            text_rect = text.get_rect(center=(CELL_SIZE//2, CELL_SIZE//2))
+            self.image.blit(text, text_rect)
+
+    def set_value(self, new_value):
+        self.value = new_value
+        self.update_color()
 
 class game2048:
     def __init__(self):
